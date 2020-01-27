f5225856d9a1fd5e006246a3e93575f6dda2b85f
Jennings Anderson <jennings.anderson@gmail.com>
2020-01-27 07:48:54 +0000

    changeset parser

--- changesets/index.js
+++ changesets/index.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 
 //TODO: Be smart about parsing the first line of the CSV as header row...
 // for now, use this: 
-const HEADERS = ["id","tags","created_at","min_lat","max_lat","min_lon","max_lon","num_changes","uid","user"];
+const HEADERS = ["id","created_at","tags","min_lat","max_lat","min_lon","max_lon","num_changes","uid","user"];
 //as generated from this
 
 /* ```sql
@@ -14,11 +14,11 @@ const HEADERS = ["id","tags","created_at","min_lat","max_lat","min_lon","max_lon
 
 streamReduce({
   map: path.join(__dirname, 'map-changeset-parser.js'),
-  file: path.join(__dirname, '../../data/changesets_2019.csv'),
+  file: path.join(__dirname, 'changesets.csv'),
 //   file: path.join(__dirname, 'ghana_changesets.csv'),
   mapOptions: {
       headers: HEADERS,
-      maxAreaKM: 1000 // square kilometers (larger than these is a bit excessive?)
+      maxAreaKM: 2500 // square kilometers (larger than these is a bit excessive?)
   },
   maxWorkers:32, // The number of cpus you'd like to use
 //   output: fs.createWriteStream('tmp.fs')  
